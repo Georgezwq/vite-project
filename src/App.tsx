3dca84c6b242766eafc0132e9b6c55d3e7643cf1
wenqiangzhang <wenqiangzhang@pharmaoryx.com>
2025-10-12 17:48:07 +0800

    feat: router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./home";
 import EntryPoint from "./EntryPoint";
 import Stock from "./Stock";
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<EntryPoint />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/home/stock" element={<Stock />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<EntryPoint />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/home/stock" element={<Stock />} />
+    </Routes>
   );
 }
 
